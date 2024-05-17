74dbe4bd364119ee116aeb2849adf796259eea69
ReginaS1987 <reginasirazieva1987@gmail.com>
2024-05-17 17:26:01 +0300

    кнопка

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-import React,{ Component } from 'react';
-import styled from 'styled-components';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
+import { Component } from 'react';
 import './App.css';
+import styled from 'styled-components';
 
 
 
-
+ export const Button = styled.button `
+        display: block;
+        padding:5 px 15px;
+        background-color:gold;
+        border:1px solid rgba(0,0, .2);
+        box-shadow: 5px 5px 10px-rgba(0,0, .2);
+`
+ 
 
 class WhoAmI extends Component {
   constructor(props) {
-    super(props);
+  super(props);
     this.state={
       years:27
     }
@@ -28,40 +33,40 @@ this.setState({
   const {name, surname,link}=this.props
   return (
     <div>
-      <button onClick= {this.nextYear}>+++</button>
+      <Button onClick= {this.nextYear}>+++</Button>
     <h1> My name is {name}, surname - {surname}, {link}, age-{this.state.years}</h1>
     <a href={link}>My profile</a>
   </div>
   )
- 
-}
-}
 
-const Wrapper = styled.div`
-    width: 600px;
-    margin: 80px auto 0 auto;
-`;
-
-const DynamicGreating = (props) => {
-    return (
-        <div className={'mb-3 p-3 border border-' + props.color}>
-           {props.children}
-        </div>
-    )
 }
+}
+const Wrapper =styled.div `
+        width:600px;
+        margin: 80px auto 0 auto;
+         `
+  
+
+
+
+// const DynamicGreating = (props) => {
+//     return (
+//         <div className={'mb-3 p-3 border border-' + props.color}>
+//            {props.children}
+//         </div>
+//     )
+// }
 
 function App() {
   return (
-    <Wrapper>
-                <DynamicGreating color={'primary'}>
-                    <h2>This weel was hard</h2>
-                    <h2>Hello world!</h2>
-                </DynamicGreating>
                 
+          <Wrapper>
+               <WhoAmI name='John' surname="Smith" link="facebook.com"/>
+              <WhoAmI name='Alex' surname="Shepard" link="vk.com"/>
+              <WhoAmI name='Piter' surname="Shepard" link="vk.com"/>
 
-        <WhoAmI name='John' surname="Smith" link="facebook.com"/>
-        <WhoAmI name='Alex' surname="Shepard" link="vk.com"/>
-    </Wrapper>
+          </Wrapper>
+
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, {StrictMode} from 'react';
 import ReactDOM from 'react-dom/client';
+
 import './index.css';
 import App from './App';
+import {Button} from './App';
+
 
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-<App/>
+    <StrictMode>
+    <App/>
+    <Button/>
+    </StrictMode>,
 );
 
